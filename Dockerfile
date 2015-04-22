6a19d44f418e9489e029b1ffce086b0b3d5b8761
Siddhartha Banerjee <banerjs.sid@gmail.com>
2015-04-21 22:00:31 -0700

    Creating a Dockerfile for potential future use. Also changing default port and response type of the app

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# First specify that we need the slim node image
+FROM node:slim
+
+# And then create a container that can be used to serve the webpages. Till then
+# do nothing
+
+# Future commands to keep in mind:
+# npm install -g react-tools webpack
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /** This file has all the API routes */
 router.get('/', function (req, res, next) {
-	res.send(JSON.stringify({ "data": "Hello World"}));
+	res.json({ "data": "Hello World"});
 });
 
 module.exports = router;
