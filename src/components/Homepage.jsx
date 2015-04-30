cf84ff75210fdc27a9ddfee0c5116e214786d6a1
Siddhartha Banerjee <banerjs.sid@gmail.com>
2015-04-30 01:53:43 -0700

    We now have a beautiful Homepage. Need a proper 'About' section now

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,55 +1,141 @@
 // Homepage
 var React = require('react');
+var Link = require('react-router').Link;
 
-// Component for the internals of the Code section
-var CodeHandler = React.createClass({
-	statics: {
-		getImage: function() {
-			return "code.jpg";
-		}
-	},
-
+// Mixin for a HomepageSectionHandler
+var HandlerMixin = {
 	style: {
 		root: {
-			color: "#fff",
-			backgroundColor: "rgba(0,0,0,0.7)",
-			marginTop: "40%",
-			borderRadius: 5
+			margin: 0,
+			padding: 0,
+			height: "100%",
+			width: "100%",
+			position: "relative"
+		},
+		div: {
+			root: {
+				color: "#fff",
+				backgroundColor: "rgba(0,0,0,0.7)",
+				borderRadius: 5,
+				position: "absolute",
+				top: "30%"
+			}
 		}
 	},
 
 	render: function() {
 		return (
-			<div className="col-sm-offset-6 col-sm-6 text-center" style={this.style.root}>
-				<h4><span className="glyphicon glyphicon-wrench"></span> Engineering</h4>
-				<h4><small>Find me on <a href="https://github.com/banerjs" target="_blank">Github</a></small></h4>
+			<div style={this.style.root}>
+				<div className="col-sm-offset-9 col-sm-3 text-center" style={this.style.div.root}>
+					<h4>{this.getTitle()}</h4>
+					<h4><small>{this.getBody()}</small></h4>
+				</div>
 			</div>
+		);
+	}
+};
+
+// Component for the internals of the Code section
+var CodeHandler = React.createClass({
+	mixins: [HandlerMixin],
+
+	statics: {
+		image: "code.jpg"
+	},
+
+	getTitle: function() {
+		return (
+			<span><span className="glyphicon glyphicon-wrench"></span> Engineering</span>
+		);
+	},
+
+	getBody: function() {
+		return (
+			<span>My projects are on <a href="https://github.com/banerjs" target="_blank">Github</a></span>
 		);
 	}
 });
 
 // Component for the internals of the Books section
 var BooksHandler = React.createClass({
+	mixins: [HandlerMixin],
+
 	statics: {
-		getImage: function() {
-			return "books.jpg";
-		}
+		image: "books.jpg"
 	},
 
-	style: {
-		root: {
-			color: "#fff",
-			height: "100%",
-			minHeight: "100%",
-			backgroundColor: "rgba(0,0,0,0.7)"
-		}
-	},
-
-	render: function() {
+	getTitle: function() {
 		return (
-			<div className="col-md-offset-6 col-md-6" style={this.style.root}>
-				<h4>Books</h4>
-			</div>
+			<span><span className="glyphicon glyphicon-book"></span> Currently Reading</span>
+		);
+	},
+
+	getBody: function() {
+		return (
+			<span>Find me on <a href="https://www.goodreads.com/user/show/41408373-siddhartha-banerjee" target="_blank">Goodreads</a></span>
+		);
+	}
+});
+
+// Component for the internals of the Nature Section
+var NatureHandler = React.createClass({
+	mixins: [HandlerMixin],
+
+	statics: {
+		image: "nature.jpg"
+	},
+
+	getTitle: function() {
+		return (
+			<span><span className="glyphicon glyphicon-tree-conifer"></span> Adventures in Nature</span>
+		);
+	},
+
+	getBody: function() {
+		return (
+			<span>Read about them in my <Link to="blogs">Blog</Link></span>
+		);
+	}
+});
+
+// Component for the internals of the Travels section
+var TravelsHandler = React.createClass({
+	mixins: [HandlerMixin],
+
+	statics: {
+		image: "travel.jpg"
+	},
+
+	getTitle: function() {
+		return (
+			<span><span className="glyphicon glyphicon-road"></span> Travels</span>
+		);
+	},
+
+	getBody: function() {
+		return (
+			<span>Check them out in my <Link to="travels">Travelogue</Link></span>
+		);
+	}
+});
+
+// Component for the internals of the Health section
+var HealthHandler = React.createClass({
+	mixins: [HandlerMixin],
+
+	statics: {
+		image: "sports.jpg"
+	},
+
+	getTitle: function() {
+		return (
+			<span><span className="glyphicon glyphicon-heart"></span> Health & Fitness</span>
+		);
+	},
+
+	getBody: function() {
+		return (
+			<span>My routines are uploaded to my <Link to="blogs">Blog</Link></span>
 		);
 	}
 });
@@ -71,11 +157,6 @@ var PageSection = React.createClass({
 			backgroundRepeat: "no-repeat",
 			backgroundAttachment: "fixed",
 			backgroundSize: "cover"
-		},
-		div: {
-			root: {
-				height: "100%"
-			}
 		}
 	},
 
@@ -90,9 +171,7 @@ var PageSection = React.createClass({
 		this.style.root.backgroundImage = this.getBackgroundImageUrl();
 		return (
 			<section style={this.style.root}>
-				<div className="container-fluid" style={this.style.div.root}>
-					{this.props.handler}
-				</div>
+				{this.props.handler}
 			</section>
 		);
 	}
@@ -118,11 +197,11 @@ var Homepage = React.createClass({
 	render: function() {
 		return (
 			<main style={this.style.root}>
-				<PageSection handler={<CodeHandler />} image={CodeHandler.getImage()} />
-				<PageSection handler={<BooksHandler />} image={BooksHandler.getImage()} />
-				<PageSection image="nature.jpg" />
-				<PageSection image="travel.jpg" />
-				<PageSection image="sports.jpg" />
+				<PageSection handler={<CodeHandler />} image={CodeHandler.image} />
+				<PageSection handler={<BooksHandler />} image={BooksHandler.image} />
+				<PageSection handler={<NatureHandler />} image={NatureHandler.image} />
+				<PageSection handler={<TravelsHandler />} image={TravelsHandler.image} />
+				<PageSection handler={<HealthHandler />} image={HealthHandler.image} />
 				<PageSection image="gaze.jpg" />
 			</main>
 		);
