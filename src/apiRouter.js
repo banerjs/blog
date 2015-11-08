207bdb695fbc70ef0157bbcf6f09d0f02b158c5d
Siddhartha Banerjee <banerjs.sid@gmail.com>
2015-11-08 00:30:43 -0500

    Doing a hard reset of this project. I'm going to try to recreate a more usable website

--- src/apiRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/** This file has all the API routes */
-router.get('/', function (req, res, next) {
-	res.json({ "data": "Hello World"});
-});
-
-module.exports = router;
